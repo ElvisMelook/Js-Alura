e5dbd8edf0601d45fa2c64abb861f11683402c5c
Elvismelook <elvismelook@gmail.com>
2023-07-04 17:28:15 -0300

    up loops

---
+++ loops/lop.js
@@ -0,0 +1,10 @@
+var listaNumero= [1,2,3,4,5,6,7,8]
+
+
+for(var i = 0; i < listaNumero.length ; i++){
+
+    console.log(i)
+    if(listaNumero[i] === 7){
+        console.log(`numero ${7} encontrado`)
+    }
+}
